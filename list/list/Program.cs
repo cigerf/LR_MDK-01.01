03c2260f2fa82ea215ddfcd569a292b51efbb549
Z <st56@WS56-6>
2025-09-20 12:09:21 +0300

    Ввод слова с клавиатуры

---
+++ list/list/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            list.Add("биба");
+            list.Add("боба");
+            list.Add("Кирилл");
+            list.Add("Богдан");
+            list.Add("Дмитрий");
+            Console.WriteLine("Введите ваше крутое словечко");
+            string input = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(input);
+            List<string> Ghoul = new List<string>();
+            list.Contains  
+        }
+    }
+}
