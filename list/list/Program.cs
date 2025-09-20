66f6daaa819d7257e3526fedb7495d802da40995
Z <st56@WS56-6>
2025-09-20 13:03:40 +0300

    сортировка

--- list/list/Program.cs
+++ list/list/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +12,22 @@ namespace list
         static void Main()
         {
             List<string> list = new List<string>();
-            list.Add("биба");
+            list.Add("пр");
             list.Add("боба");
             list.Add("Кирилл");
             list.Add("Богдан");
             list.Add("Дмитрий");
+
             Console.WriteLine("Введите ваше крутое словечко");
             string input = Convert.ToString(Console.ReadLine());
-            Console.WriteLine(input);
-            List<string> Ghoul = new List<string>();
-            list.Contains  
+            Console.WriteLine("ваше крутое слово " + input);
+
+            List<string> spisok = new List<string>();
+            foreach (string element in spisok)
+            {
+                element.Contains("пр");
+                Console.Write(element);
+            }
         }
     }
 }
