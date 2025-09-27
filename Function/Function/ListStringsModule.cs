d06174ce5aad0f9809945e340aef8cef0b02f129
Daniil5903 <st50@WS50-07>
2025-09-27 13:03:36 +0300

    реализовал функццию

---
+++ Function/Function/ListStringsModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    class ListStringsModule
+    {
+        static public List<string> FillingListLines()
+        {
+            Console.WriteLine("Введите количество строк");
+            int CountLines = Convert.ToInt32(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int index = 0; index < CountLines; index++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            return list;
+        }
+        static public List<int> ElementsNumber(List<string> list, string text)
+        {
+            List<int> result = new List<int>();
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (list.Contains(text))
+                {
+                    result.Add(index);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Function/Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> value = ListStringsModule.FillingListLines();
+            Console.WriteLine();
+            Console.WriteLine("Размер списка: " + value.Count);
+            Console.WriteLine("последний элемент списка: " +value[value.Count - 1]);
+        }
+    }
+}
