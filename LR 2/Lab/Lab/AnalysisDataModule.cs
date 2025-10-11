820cb2ae390d2513d0b26328d0cc533888c50930
Z <pgbc303@gmail.com>
2025-10-11 13:00:09 +0300

    Добавил модуль AnalysisDataModule и реализовал функцию SortCustomers для сортировки посетителей в порядке убывания

---
+++ LR 2/Lab/Lab/AnalysisDataModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class AnalysisDataModule
+    {
+        static public void SortCustomers(List<string> Customers, List<int> counts)
+        {
+            for (int i = 0; i < counts.Count; ++i)
+            {
+                for (int j = 0; j < counts.Count - 1; ++j)
+                {
+                    bool condition = counts[j + 1] > counts[j];
+                    if (condition)
+                    {
+                        int temp_counts = counts[j];
+                        counts[j] = counts[j + 1];
+                        counts[j + 1] = temp_counts;
+
+                        string temp_Customers = Customers[j];
+                        Customers[j] = Customers[j + 1];
+                        Customers[j + 1] = temp_Customers;
+                    }
+                }
+            }
+        }
+    }
+}
--- LR 2/Lab/Lab/Program.cs
+++ LR 2/Lab/Lab/Program.cs
@@ -13,6 +13,13 @@ namespace Lab
             string[] services = new string[] { "Массаж", "Маникюр", "Педикюр" };
             var (customers, counts) = InputModule.InputCustomersByServices();
             string userQuery = InputModule.InputUserQuery();
+            int indexService = SearchingModule.FindIndexServices(userQuery, services);
+            if (indexService < 0)
+            {
+                Console.WriteLine("Указан несуществующий жанр");
+                return;
+            }
+            var (CustomersUserServices, countsUserServices) = SearchingModule.FindAllCustomersByServices(indexService, customers, counts);
         }
     }
 }
