d43e3edabf0bfd4869f31f66b75fce245bad179b
Z <pgbc303@gmail.com>
2025-10-11 12:51:13 +0300

    Добавил модуль SearchingModule и реализовал функцию FindIndexServices для поиска индекса услуги

---
+++ LR 2/Lab/Lab/SearchingModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class SearchingModule
+    {
+        static public int FindIndexServices(string userQuery, string[] services)
+        {
+            for (int index = 0; index < services.Length; ++index)
+            {
+                string valueServices = services[index].ToLower();
+                if (valueServices == userQuery.ToLower())
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
