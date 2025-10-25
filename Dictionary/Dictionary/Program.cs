a7a4451d4f6ff10b260dd9ec18815fba3ed99ff9
KateSmirnova1 <20katerina0060@gmail.com>
2025-10-25 13:01:51 +0300

    работа с словарями

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static public void NumberElements(Dictionary<string, List<string>> magazines)
+        {
+            Console.WriteLine("Введите журнал:");
+            string key = Console.ReadLine(); // ввод ключа
+            if (magazines.ContainsKey(key))
+            {
+                int number = magazines[key].Count(); // Поиск числа элементов
+                Console.Write(number);
+            }
+        }
+        static void Main()
+        {
+            Dictionary<string, List<string>> magazines = new Dictionary<string, List<string>>(); // создал пустой словарь
+            magazines.Add("Звезда", new List<string>() {"Гадание", "Шоу-бизнес"}); // Добавили ключ "Звезда" и заполнили значениями
+            magazines.Add("Луна", new List<string>() {"Календарь огородникам","Программа радио"}); // аналогично с верхней
+            magazines.Add("Ретроградный меркурий", new List<string>() {"Воскресенский воскрес в Воскресенье", "Гуф вернулся к жизни"}); // аналогично с верхней
+            Console.WriteLine("Ключи: " +string.Join(", ", magazines.Keys));//вывод ключей 
+            /*
+             Console.WriteLine();
+             Console.WriteLine("Введите ключ:");
+             string key = Console.ReadLine(); // ввод ключа
+             Console.WriteLine();
+             Console.WriteLine(string.Join(", ", magazines[key])); // ввывод по введёному ключу
+            */
+            NumberElements(magazines);
+        }
+    }
+}
