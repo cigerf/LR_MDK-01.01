09341d9bd149e6d5dbfcbb8a77cbe3bec1d31fc5
st56 <skadeshopp@gmail.com>
2026-02-03 11:16:54 +0300

    Готовое задание(некрасиво)

---
+++ WindowsFormsApp2/WindowsFormsApp2/FileUsersStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class FileUsersStorage : IUserInterface
+    {
+        public List<User> Load()
+        {
+            List<User> result = new List<User>();
+            StreamReader Sr = new StreamReader("D:\\Воск repos\\WindowsFormsApp2\\Parolle.txt");
+            string line;
+            while ((line = Sr.ReadLine()) !=null)
+            {
+                string[] UserInformation = line.Split('-');
+                User user = new User(UserInformation[0], UserInformation[1]);
+                result.Add(user);
+            }
+            return result;
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/UserInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    interface IUserInterface
+    {
+        List<User> Load();
+
+    }
+}
