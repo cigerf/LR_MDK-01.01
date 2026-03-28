c73bb86f3b22a3dccc71286e9fa786d6bc98da8e
GavrishM <nanotechnology930@gmail.com>
2026-03-28 09:33:58 +0300

    Работа на паре

---
+++ TestFileStorage/TestFileStorage/Registration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFileStorage
+{
+    public partial class Registration: Form
+    {
+        private UsersDataBase registration = new UsersDataBase();
+        
+        public Registration()
+        {
+            InitializeComponent();
+
+        }
+
+        private void RegistrationButton_Click(object sender, EventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordTextBox.Text;
+            User user = new User(login, password);
+            if (password == null)
+            {
+                MessageBox.Show("Вы не ввели пароль");
+                return;
+            }
+
+            if (registration.UserVerification(login))
+            {
+                MessageBox.Show("Такой пользователь уже существует");
+            }
+            else
+            {
+                if (registration.UserRegistration(user))
+                {
+                    MessageBox.Show("Вы успешно зарегистрировались");
+                }
+            }
+        }
+        
+    }
+}
---
+++ TestFileStorage/TestFileStorage/UsersDataBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace TestFileStorage
+{
+    class UsersDataBase : IUsersInterface
+    {
+        List<User> AllUsers = new List<User>();
+        private const string connectSetting = "Host=192.168.1.48;Username=st50-10;Password=5010;Database=test01";
+        public List<User> Load()
+        {
+            try
+            {
+                var con = new NpgsqlConnection(connectSetting);
+                con.Open();
+                var sql = "SELECT login, password FROM users";
+                var cmd = new NpgsqlCommand(sql, con);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                        User user = new User(reader.GetString(0), reader.GetString(1));
+                    AllUsers.Add(user);
+                }
+                return AllUsers;
+            }
+            catch (NpgsqlException exception)
+            {
+                MessageBox.Show($"Ошибка: {exception.Message}");
+                return null;
+            }
+        }
+        public bool UserRegistration(User us)
+        {
+            try
+            {
+                bool addResult = false;
+                var con = new NpgsqlConnection(connectSetting);
+                con.Open();
+                var sql = "INSERT INTO users(login, password) VALUES(@login, @password)";
+                var cmd = new NpgsqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@login", us.Login);
+                cmd.Parameters.AddWithValue("@password", us.Password);
+                int execute = cmd.ExecuteNonQuery();
+                if (execute > 0)
+                {
+                    addResult = true;
+                    AllUsers.Add(us);
+                }
+                return addResult;
+            }
+            catch (NpgsqlException exception)
+            {
+                MessageBox.Show($"Ошибка: {exception.Message}");
+                return false;
+            }
+        }
+
+        public bool UserVerification(string login)
+        {
+            try
+            {
+                bool result = false;
+                var con = new NpgsqlConnection(connectSetting);
+                con.Open();
+                var sql = "SELECT EXISTS(SELECT 1 FROM users WHERE login = @login)";
+                var cmd = new NpgsqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@login", login);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader.GetBoolean(0) == true) result = true;
+                }
+                return result;
+            }
+            catch (NpgsqlException exception)
+            {
+                MessageBox.Show($"Ошибка: {exception.Message}");
+                return false;
+            }
+        }
+    }
+}
